63c0b16a30548d1f5c5cd0df060311652bc3d3b9
Iamneo <techadmin@iamneo.ai>
2023-10-02 07:13:21 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
+++ springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
@@ -11,20 +11,27 @@ public class LaptopController {
     public boolean createLaptop() {
         // Implement logic to create a laptop and return true or false based on success.
         return laptops.add(laptop);
-        return true; // or false
+       // or false
     }
 
     @GetMapping("/")
     public List<Laptop> getAllLaptops() {
         // Implement logic to retrieve a list of laptops and return them.
+        return laptops;
         // You may need to import the Laptop class and implement the logic.
-        return null; // Return a list of laptops or null if not implemented yet.
+     // Return a list of laptops or null if not implemented yet.
     }
 
     @GetMapping("/{laptopId}")
     public Laptop getLaptopById(@PathVariable int laptopId) {
         // Implement logic to retrieve a laptop by its ID and return it.
         // You may need to import the Laptop class and implement the logic.
-        return null; // Return a laptop or null if not implemented yet.
+       // Return a laptop or null if not implemented yet.
+       for(Laptop laptop:laptops){
+        if(laptop.getLaptopId()==laptopId){
+            return laptop;
+        }
+    }
+    return null;
     }
 }
