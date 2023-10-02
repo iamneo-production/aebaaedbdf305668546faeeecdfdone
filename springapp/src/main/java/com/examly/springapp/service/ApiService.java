fc9c0c2496ea73e6909a2670d938db63feb92b84
Iamneo <techadmin@iamneo.ai>
2023-10-02 06:38:21 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/service/ApiService.java
+++ springapp/src/main/java/com/examly/springapp/service/ApiService.java
@@ -1,4 +1,26 @@
 import java.util.*;
 public class ApiService {
-    private List<Laptop>
+    private List<Laptop> laptops;
+
+    public ApiService(){
+        laptops=new ArrayList<>();
+    }
+    //
+    public void addLaptop(Laptop laptop){
+        laptops.add(laptop);
+    }
+    public Laptop getLaptopById(int laptopId){
+        for(Laptop laptop:laptops){
+            if(laptop.getLaptopId()==laptopId){
+                return laptop;
+            }
+        }
+        return null;
+    }
+    public List<Laptop>getAllLaptops(){
+        return laptops;
+    }
+    public void removeLaptopById(int laptopId){
+        laptops.removeIf(laptop->laptop.getLaptopId()==laptopId);
+    }
 }
